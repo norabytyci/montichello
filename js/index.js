d538d5809460033b5b13c5674a5bb75580244cc1
Nora Bytyci <nora.bytyci@student.uni-pr.edu>
2021-04-18 13:00:58 +0200

    Add: News and Gallery section

--- js/index.js
+++ js/index.js
@@ -9,4 +9,12 @@ $(document).ready(function() {
     $(".header__scroll button").on("click", function() {
         $("html, body").animate({ scrollTop: $(document).height() }, 1000);
     })
+
+    $(".news__slider").slick({
+        dots: true,
+        arrows: true,
+        slidesToShow: 3,
+        prevArrow: $('.news__arrow--left'),
+        nextArrow: $('.news__arrow--right')
+    });
 });
