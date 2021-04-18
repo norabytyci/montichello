95473e73674f4b9f3e5ea49c46b2a45287e0d9a1
Nora Bytyci <nora.bytyci@student.uni-pr.edu>
2021-04-18 14:40:05 +0200

    FINISHED

--- js/index.js
+++ js/index.js
@@ -5,14 +5,15 @@ $(document).ready(function() {
         arrows: false
     });
     $(".slick-dots").insertBefore($(".header__slider"));
-
     $(".header__scroll button").on("click", function() {
-        $("html, body").animate({ scrollTop: 800 }, 500);
+        $("html, body").animate({ scrollTop: $("#gallery").offset().top - 50 }, 500);
     })
 
     $(".news__slider").slick({
         dots: true,
         arrows: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
         slidesToShow: 3,
         prevArrow: $('.news__arrow--left'),
         nextArrow: $('.news__arrow--right')
